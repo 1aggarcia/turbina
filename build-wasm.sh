c92dd289d0c22de3b61fb1f58c82fa3e9d460c83
Apolo Garcia <apolo270garcia@gmail.com>
2025-02-08 11:41:52 -0800

    Fix WASM compilation error caused by rand crate

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-wasm-pack build --target web --out-dir online-playground/pkg
+# compiles Turbina to a Web Assembly package in the `online-playground` directory
+RUSTFLAGS='--cfg getrandom_backend="wasm_js"' wasm-pack build --target web --out-dir online-playground/pkg
